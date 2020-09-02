6ac1eb43c7331df7ecfcec5259bcb9a9c4d62d08
Dain LaRock <DainTrain00@gmail.com>
2020-09-01 22:19:19 -0500

    adding main functionality

--- public/ankicon.js
+++ public/ankicon.js
@@ -1,5 +1,40 @@
 function addToAnki() {
-    
+    let wordContent = "";
+    for (j = 0; j < selectsArray.length; j++) {
+        if (selectsArray[j].value == "on") {
+            wordContent += definitionsArray[j] + "; ";
+        }
+    }
+    let word = $("#wordInput")[0].value;
+    let wordObj = {
+        "action": "addNote",
+        "version": 6,
+        "params" : {
+            "note" : {
+                "deckName" : "testdeck",
+                "modelName" : "Basic (and reversed card)",
+                "fields" : {
+                    "Front": word,
+                    "Back": wordContent
+                },
+                "options" : {
+                    "allowDuplicate": false,
+                    "duplicateScope": "deck"
+                }
+            }
+        }
+    };
+    $.ajax({
+        url: "http://127.0.0.1:8765",
+        method: "POST",
+        data: JSON.stringify(wordObj),
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
 }
 
 function getDecks() {
--- public/merweb.js
+++ public/merweb.js
@@ -3,12 +3,54 @@ let THES_API_TOKEN = "";
 let dictionaryUrl = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
 let thesaurusUrl = "";
 
+let selectsArray = [];
+let definitionsArray = [];
+
+$.ajax({
+    url: "http://localhost:8766/apikeys",
+    method: "GET",
+    success: function(result) {
+        let apiKeys = JSON.parse(result);
+        DICT_API_TOKEN = apiKeys.dictionaryApiToken;
+        console.log('here', DICT_API_TOKEN);
+    }
+})
+
 function fetchDefinition() {
-    console.log('fetc');
     let word = document.getElementById("wordInput").value;
     $.ajax({
         url: dictionaryUrl + word + "?key=" + DICT_API_TOKEN,
         success: function(result) {
+            console.log(result);
+
+            let definitionsTable = document.getElementById("definitionsTable");
+            for (i = 0; i < result.length; i++) {
+                let tr = document.createElement("tr");
+                let selectTd = document.createElement("td");
+                let select = document.createElement("input");
+                select.type = "checkbox";
+                select.value = "off";
+                select.onclick = function() {
+                    this.value = "on";
+                }
+                select.setAttribute("id", "select" + i);
+                select.setAttribute("style", "text-align: center; vertical-align: middle");
+                selectTd.appendChild(select);
+
+                selectsArray[i] = select;
+                definitionsArray[i] = result[i].shortdef;
+
+                let definition = document.createElement("td");
+                definition.innerHTML = result[i].shortdef;
+                definition.setAttribute("id", "definition" + i);
+                tr.appendChild(select);
+                tr.appendChild(definition);
+
+                definitionsTable.appendChild(tr);
+                
+            }
+        },
+        error: function(result) {
             console.log(result);
         }
     });
